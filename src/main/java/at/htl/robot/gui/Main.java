5ceee5bef81b5048164cf809085d53f013bd8a83
QuirinEcker <33062936+QuirinEcker@users.noreply.github.com>
2018-10-13 01:20:33 +0200

    finished Quirin Ecker

--- src/main/java/at/htl/robot/gui/Main.java
+++ src/main/java/at/htl/robot/gui/Main.java
@@ -8,6 +8,14 @@ public class Main extends PApplet {
 
     // Hier die Member-Attribute eintragen
 
+    private float upperMargin = 50;
+    private float leftMargin = 50;
+    private float celllength = 50;
+    private float tableLength = celllength * 10;
+    private Robot robot = new Robot();
+    private int x = 0;
+    private int y = 0;
+
     public static void main(String[] args) {
         PApplet.main("at.htl.robot.gui.Main", args);
     }
@@ -18,34 +26,47 @@ public class Main extends PApplet {
 
     public void setup() {
         background(209); //https://processing.org/tutorials/color/
-
-
+        robot.setY(0);
+        robot.setX(0);
     }
 
     /**
      * Diese Methode wird iterativ durchlaufen (wie loop() beim Arduino)
      */
     public void draw() {
+        deleteAll();
+        drawtable();
+        drawRobot();
+    }
 
+    private void drawtable() {
+        for (int i = 1; i < 12; i++) {
+            line(leftMargin, i * upperMargin, leftMargin + tableLength, i * upperMargin);
+            line(leftMargin * i, upperMargin, leftMargin * i, upperMargin + tableLength);
+        }
     }
 
     /**
      * Erstellen Sie eine eigene Methode, mittels der der Roboter am Bildschirm gezeichnet wird
      * Die Angabe zu Position des Roboters am Spielfeld erhalten Sie aus dem Roboter-Objekt, welches
      * als Parameter übergeben wird.
-     *
-     * @param robot Objekt des zu zeichnenden Roboters
      */
-    public void drawRobot(Robot robot) {
-
-
+    private void drawRobot() {
+        if (x < 0 || y < 0) {
+            if (x < 0) {
+                x = 0;
+            } else {
+                y = 0;
+            }
+        }
+        ellipse(leftMargin + (celllength / 2) + (celllength * x), upperMargin + (celllength / 2) + (celllength * y), celllength, celllength);
     }
 
     /**
      * Erstellen Sie eine eigene Methode zum Löschen des Bildschirms
      */
-    public void deleteAll() {
-
+    private void deleteAll() {
+        background(209);
     }
 
     /**
@@ -55,19 +76,16 @@ public class Main extends PApplet {
         println("pressed " + key + " " + keyCode);
 
         if (key == 'f' || key == 'F') {
-
+            robot.stepForward();
+            x = robot.getX();
+            y = robot.getY();
         } else if (key == 'l' || key == 'L') {
-
+            robot.rotateLeft();
         }
 
     }
-
-//    public void keyTyped() {
-//        println("typed " + key + " " + keyCode);
-//    }
-//
-//    public void keyReleased() {
-//        println("released " + key + " " + keyCode);
-//    }
-
 }
+
+
+
+
--- src/main/java/at/htl/robot/model/Robot.java
+++ src/main/java/at/htl/robot/model/Robot.java
@@ -1,7 +1,59 @@
 package at.htl.robot.model;
 
 public class Robot {
+    private int y;
+    private int x;
+    private Direction direction = Direction.SOUTH;
 
 
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void stepForward() {
+        switch (this.direction) {
+            case SOUTH: y++;
+                break;
+            case NORTH: y--;
+                break;
+            case EAST: x++;
+                break;
+            case WEST: x--;
+                break;
+        }
+    }
+
+    public void rotateLeft() {
+        switch (this.direction) {
+            case SOUTH: this.direction = Direction.EAST;
+                break;
+            case NORTH: this.direction = Direction.WEST;
+                break;
+            case EAST: this.direction = Direction.NORTH;
+                break;
+            case WEST: this.direction = Direction.SOUTH;
+                break;
+        }
+    }
 
 }
